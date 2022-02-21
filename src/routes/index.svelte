3981535e8309cebce3e2334fb50dea24a6407749
this-is-just-typical <natchanon.ken135@gmail.com>
2022-02-21 18:53:44 +0700

    init project

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Banner from "../banner.svelte";
+    import Sidebar from "../sidebar.svelte";
+</script>
+<style lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
+<Banner/>
+<h1 class="text-center">Welcome to a new project!</h1>
+<Sidebar/>
+<a href="/about">about us.</a>
