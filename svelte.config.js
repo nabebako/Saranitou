040be58994cae9c0349898dcd99ac5943e882f71
this-is-just-typical <natchanon.ken135@gmail.com>
2022-02-28 12:06:38 +0700

    config change

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
+
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	
-	preprocess: preprocess(),
+	preprocess: [preprocess({
+		postcss: { plugins: [autoprefixer()] },
+		defaults: { script: 'typescript', style: 'postcss' }
+	})],
 
 	kit: {
 		adapter: adapter(),
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          '100': '#1fb6ff',
+          '200': '#aabbff',
+        }
+      }
+    },
   },
   plugins: [],
 }
