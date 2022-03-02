24bc7589a9f18f103435e9aaeba1343c22f0dcf4
this-is-just-typical <natchanon.ken135@gmail.com>
2022-03-02 17:19:41 +0700

    fixed img link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,20 +8,19 @@
 <svelte:head>
 	<title>Untitled</title>
 	<meta charset="utf-8" />
-	<link rel="icon" href="%svelte.assets%/favicon.jpg" />
+	<link rel="icon" href="https://untitled-umber.vercel.app/favicon.jpg" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 	<meta property="og:title" content="Untitled Project" />
 	<meta property="og:description" content="This is a simple food suggestion website, currently in the prototype phase." />
-	<meta property="og:image" content="%svelte.assets%/favicon.jpg" />
+	<meta property="og:image" content="https://untitled-umber.vercel.app/favicon.jpg" />
 	<meta property="og:url" content="https://untitled-umber.vercel.app" />
 
 	<meta name="twitter:card" content="summary_large_image">
-	<meta name="twitter:site" content="https://untitled-umber.vercel.app">
 	<meta name="twitter:creator" content="@ken_tummada">
 	<meta name="twitter:title" content="Untitled Project">
 	<meta name="twitter:description" content="An unnamed food suggestion project.">
-	<meta name="twitter:image" content="%svelte.assets%/favicon.jpg">
+	<meta name="twitter:image" content="https://untitled-umber.vercel.app/favicon.jpg">
 </svelte:head>
 
 <header>
