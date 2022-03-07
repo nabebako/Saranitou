3368c4cfd2fad1657e182b99cfda6f4143d3a6a6
this-is-just-typical <natchanon.ken135@gmail.com>
2022-03-07 12:44:31 +0700

    fix error

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
 					'button-color': 'rgb(51 65 85)',
 					'button-text': 'rgb(241 245 249)',
 					'border': 'rgb(15 23 42/.1)',
+					'checked': '',
 				},
 				dark: {
 					'background': 'rgb(30 41 59)',
@@ -17,10 +18,18 @@ module.exports = {
 					'button-color': 'rgb(148 163 184)',
 					'button-text': 'rgb(30 41 59)',
 					'border': 'rgb(203 213 225/.1)',
+					'checked': '',
+				},
+				button: {
+					'bg': 'rgb(51 65 85)',
+					'text': 'rgb(241 245 249)',
 				}
 			},
 			spacing: {
 				'80vw': '80vw',
+			},
+			transitionProperty: {
+				'height': 'height',
 			}
 		}
 	},
