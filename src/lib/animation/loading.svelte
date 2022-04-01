819b8c43e1f40f713bb713537aabf5890b4be481
this-is-just-typical <natchanon.ken135@gmail.com>
2022-04-01 11:23:58 +0700

    added loading animation

---
+++ src/lib/animation/loading.svelte
@@ -0,0 +1,134 @@
+<div class="w-fit text-center">
+	<svg
+		class="overflow-visible"
+		width="240"
+		height="240"
+		viewBox="0 0 241 240"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<g class="loading">
+			<g class="cutting-bord">
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M9.22089 111.058C4.41405 113.777 2.72199 119.878 5.44157 124.685L53.8908 210.319C56.6104 215.126 62.7118 216.818 67.5186 214.098L217.977 128.973C222.784 126.253 224.476 120.152 221.757 115.345L173.307 29.7114C170.588 24.9046 164.486 23.2125 159.68 25.9321L9.22089 111.058ZM180.53 104.757C181.346 106.199 183.176 106.707 184.618 105.891L191.496 102L191.51 101.992C192.942 101.172 193.443 99.3486 192.63 97.9113L172.059 61.5517C171.243 60.1097 169.413 59.602 167.971 60.4179L161.092 64.3096C159.655 65.1227 159.146 66.9435 159.95 68.3833L159.958 68.3979L180.53 104.757Z"
+					fill="url(#paint0_linear_37_207)"
+				/>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M159.281 26.1172C164.088 23.3976 170.189 25.0896 172.909 29.8965L216.534 107.003C219.254 111.81 217.562 117.912 212.755 120.631L66.907 203.148C62.1002 205.867 55.9988 204.175 53.2792 199.369L9.65421 122.262C6.93463 117.455 8.62669 111.353 13.4335 108.634L159.281 26.1172ZM180.53 104.757C181.346 106.2 183.176 106.707 184.618 105.891L191.496 102L191.51 101.992C192.942 101.172 193.443 99.3487 192.63 97.9114L172.059 61.5518C171.243 60.1097 169.413 59.6021 167.971 60.418L161.092 64.3096C159.655 65.1227 159.146 66.9436 159.95 68.3834L159.958 68.398L180.53 104.757Z"
+					fill="#433C44"
+				/>
+				<path
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+					d="M157.994 58.8334C156.552 59.6492 156.044 61.4796 156.86 62.9217L159.95 68.3833C159.146 66.9435 159.655 65.1226 161.092 64.3095L167.971 60.4179C169.413 59.602 171.243 60.1096 172.059 61.5517L192.63 97.9113C193.444 99.3486 192.942 101.172 191.511 101.992L194.186 100.478C195.628 99.6623 196.135 97.8319 195.319 96.3898L171.65 54.5541C170.834 53.1121 169.004 52.6045 167.561 53.4203L157.994 58.8334Z"
+					fill="url(#paint1_linear_37_207)"
+				/>
+			</g>
+			<g class="knife">
+				<path
+					d="M159.003 140.368L166.625 118.519C166.737 118.202 166.486 117.82 166.367 117.661C165.998 117.169 165.87 117.149 164.752 116.753L81.289 87.1975L79.9623 86.7278C79.6865 86.6301 79.8232 86.769 79.8676 86.9952C80.0817 87.5177 80.3239 88.0459 80.5929 88.5791L81.6941 90.5506C93.6063 110.061 138.02 135.312 157.439 142.429C158.005 142.611 158.087 142.505 158.141 142.476C158.182 142.454 158.355 142.2 158.479 141.848L159.003 140.368Z"
+					fill="url(#paint2_linear_37_207)"
+				/>
+				<path
+					d="M166.608 118.482L158.987 140.331C185.716 149.419 220.298 162.447 227.431 163.604C232.555 164.855 238.112 150.606 238.102 149.189C238.088 147.157 238.021 144.296 234.686 143.115C234.686 143.115 175.446 121.612 166.608 118.482Z"
+					fill="url(#paint3_linear_37_207)"
+				/>
+			</g>
+		</g>
+		<defs>
+			<linearGradient
+				id="paint0_linear_37_207"
+				x1="41.997"
+				y1="72.7959"
+				x2="35.5573"
+				y2="264.357"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop offset="0.0001" stop-color="#433C44" />
+				<stop offset="1" stop-color="#E4687C" />
+			</linearGradient>
+			<linearGradient
+				id="paint1_linear_37_207"
+				x1="123.176"
+				y1="79.6558"
+				x2="256.886"
+				y2="56.351"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="#E4687C" />
+				<stop offset="1" stop-color="#5D535E" stop-opacity="0" />
+			</linearGradient>
+			<linearGradient
+				id="paint2_linear_37_207"
+				x1="172.089"
+				y1="100.406"
+				x2="61.5835"
+				y2="220.541"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="#ECE9EC" />
+				<stop offset="1" stop-color="#E4687C" />
+			</linearGradient>
+			<linearGradient
+				id="paint3_linear_37_207"
+				x1="189.055"
+				y1="134.796"
+				x2="112.939"
+				y2="223.751"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="#433C44" />
+				<stop offset="1" stop-color="#E4687C" />
+			</linearGradient>
+		</defs>
+	</svg>
+	<p class="loading-text">loading</p>
+</div>
+
+<style>
+	@keyframes loading {
+		from {
+			transform: translate(30%, -40%) rotate(20deg);
+			opacity: 0;
+		}
+		20% { opacity: 1; }
+		50% {
+			transform: translate(0px, 0px) rotate(0deg);
+			opacity: 1;
+		}
+		80%, 100% { opacity: 0; }
+	}
+
+	@keyframes text-loading {
+		0% {
+			content: '';
+		}
+		40% {
+			content: '.';
+		}
+		70% {
+			content: '..';
+		}
+		100% {
+			content: '...';
+		}
+	}
+
+	.knife {
+		animation-duration: 1200ms;
+	  	animation-name: loading;
+	 	animation-iteration-count: infinite;
+	  	animation-direction: normal;
+	}
+	.loading-text::after {
+		content: '';
+		animation-duration: 2400ms;
+	  	animation-name: text-loading;
+	 	animation-iteration-count: infinite;
+	  	animation-direction: normal;
+	}
+</style>
