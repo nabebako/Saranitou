080c4725be4c8606a8e50aaac2071d465be5f125
this-is-just-typical <natchanon.ken135@gmail.com>
2022-04-29 17:58:10 +0700

    implement firebase
    switch from writing suggestion to files on the server to writing to firebase

--- src/routes/api/dish/add.ts
+++ src/routes/api/dish/add.ts
@@ -1,12 +1,22 @@
 import type { RequestHandler } from '@sveltejs/kit';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, updateDoc } from 'firebase/firestore';
 
-import { db } from '$static/dummy-data/menu';
-import { writeFileSync, mkdirSync, existsSync } from 'fs';
-import path from 'path';
+const firebaseConfig = {
+  apiKey: 				import.meta.env.VITE_API_KEY as string,
+  authDomain: 			import.meta.env.VITE_AUTH_DOMAIN as string,
+  projectId: 			import.meta.env.VITE_PROJECT_ID as string,
+  storageBucket: 		import.meta.env.VITE_STORAGE_BUCKET as string,
+  messagingSenderId:	import.meta.env.VITE_MESSAGING_SENDER_ID as string,
+  appId: 				import.meta.env.VITE_APP_ID as string,
+  measurementId: 		import.meta.env.VITE_MEASUREMENT_ID as string,
+};
+
+const firestore = getFirestore(initializeApp(firebaseConfig));
 
 /** @type {import('@sveltejs/kit').RequestHandler}*/
 export const post: RequestHandler = async function ({ request }) {
-	if(request.headers.get('Content-Type').search('multipart/form-data') === -1) {
+	if(!request.headers.get('Content-Type') || request.headers.get('Content-Type')?.search('multipart/form-data') === -1) {
 		return {
 			status: 400,
 			body: {
@@ -19,35 +29,26 @@ export const post: RequestHandler = async function ({ request }) {
 		const formData 		= await request.formData();
 		const name 			= formData.get('dish-name') as string;
 		const description 	= formData.get('description') as string;
-		const imageFile 	= formData.get('image-file') as Blob;
 		const imageUrl 		= formData.get('image-url') as string;
-
-		let id = db.menu.length;
-		let dir = `src/routes/dish/${id}`;
 
 		if(!name || !description) throw new Error('no name or description provided');
 
-		while(existsSync(dir)) {
-			id++;
-			dir = `src/routes/dish/${id}`;
-		}
-
-		if (!existsSync(dir)) mkdirSync(dir);
-
-		writeFileSync(
-			path.resolve(`${dir}/${name}.svelte`),
-			`<script context='module'>\n\texport const prerendering = true;\n</script>\n<script>\n\timport Dish from "$lib/template/dish.svelte";\n</script>\n<Dish name='${name}' description='${description}'/>`,
-			'utf-8'
-		);
+		const docRef = await addDoc(collection(firestore, 'suggestions'), {
+			name,
+			description,
+			'create-time': new Date().toDateString(),
+			'image-url': null,
+			'image-type': '',
+		});
 
 		if(imageUrl) {
 			const imageTest = await fetch(imageUrl);
 			if(imageTest.status === 200 && imageTest.headers.get('content-type').match(/image\/.*/) !== null) {
 				const imageType = imageTest.headers.get('content-type').match(/(?<=image\/)[a-z]*/i);
-				writeFileSync(
-					path.resolve(`${dir}/${name}.${imageType}`),
-					Buffer.from(await imageTest.arrayBuffer())
-				);
+				updateDoc(docRef, {
+					'image-url': imageUrl,
+					'image-type': imageType,
+				});
 			} else {
 				return {
 					status: 400,
@@ -57,18 +58,6 @@ export const post: RequestHandler = async function ({ request }) {
 					},
 				}
 			} 
-		} else if(imageFile) {
-			writeFileSync(
-				path.resolve(`${dir}/${name}.${imageFile.type.split('/')[1]}`),
-				Buffer.from(await imageFile.arrayBuffer())
-			);
-		} else {
-			return {
-				status: 200,
-				body: {
-					'warn': 'no image',
-				},
-			};
 		}
 		return {
 			status: 200,
