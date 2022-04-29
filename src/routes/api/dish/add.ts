cbbcefb12f8e9a4334ddeb650ce8f909bd6e480c
this-is-just-typical <natchanon.ken135@gmail.com>
2022-04-29 16:42:26 +0700

    refactor

--- src/routes/api/dish/add.ts
+++ src/routes/api/dish/add.ts
@@ -6,59 +6,77 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').RequestHandler}*/
 export const post: RequestHandler = async function ({ request }) {
+	if(request.headers.get('Content-Type').search('multipart/form-data') === -1) {
+		return {
+			status: 400,
+			body: {
+				errors: 'invalid content type; accepts multipart/form-data',
+			},
+		}
+	}
 	try {
-		if(request.headers.get('Content-Type').search('multipart/form-data') === -1) {
-			return {
-				status: 400,
-				body: {
-					errors: 'invalide content type',
-				}
-			}
-		} else {
-			// request.headers.get('auth-token');
-			const formData = await request.formData();
-			const imageFile = formData.get('image-file') as Blob;
-			const imageUrl = formData.get('image-url') as string;
-			const name = formData.get('dish-name');
-			const description = formData.get('description');
+		// request.headers.get('auth-token');
+		const formData 		= await request.formData();
+		const name 			= formData.get('dish-name') as string;
+		const description 	= formData.get('description') as string;
+		const imageFile 	= formData.get('image-file') as Blob;
+		const imageUrl 		= formData.get('image-url') as string;
 
-			// if(!name || !description) throw new Error('no name or description provided');
-			const imageTest = await fetch(imageUrl, {
-				redirect: 'manual'
-			});
-			if(imageTest.status === 200 && imageTest.headers.get('content-type').search('image') !== -1) {
-				console.log(Buffer.from(await imageTest.arrayBuffer()));
+		let id = db.menu.length;
+		let dir = `src/routes/dish/${id}`;
 
-				let id = db.menu.length;
-				let dir = `src/routes/dish/${id}`;
-				while(existsSync(dir)) {
-					id++;
-					dir = `src/routes/dish/${id}`;
-				}
-	
-				if (!existsSync(dir)) mkdirSync(dir);
-	
-				writeFileSync(path.resolve(`${dir}/${name}.svelte`), `<script context='module'>\n\texport const prerendering = true;\n</script>\n<script>\n\timport Dish from "$lib/template/dish.svelte";\n</script>\n<Dish name='${name}' description='${description}'/>`, 'utf-8');
-				
-				
-				writeFileSync(path.resolve(`${dir}/${name}.${imageFile.type.split('/')[1]}`), Buffer.from(await imageFile.arrayBuffer()));
-				return {
-					status: 200
-				};
+		if(!name || !description) throw new Error('no name or description provided');
+
+		while(existsSync(dir)) {
+			id++;
+			dir = `src/routes/dish/${id}`;
+		}
+
+		if (!existsSync(dir)) mkdirSync(dir);
+
+		writeFileSync(
+			path.resolve(`${dir}/${name}.svelte`),
+			`<script context='module'>\n\texport const prerendering = true;\n</script>\n<script>\n\timport Dish from "$lib/template/dish.svelte";\n</script>\n<Dish name='${name}' description='${description}'/>`,
+			'utf-8'
+		);
+
+		if(imageUrl) {
+			const imageTest = await fetch(imageUrl);
+			if(imageTest.status === 200 && imageTest.headers.get('content-type').match(/image\/.*/) !== null) {
+				const imageType = imageTest.headers.get('content-type').match(/(?<=image\/)[a-z]*/i);
+				writeFileSync(
+					path.resolve(`${dir}/${name}.${imageType}`),
+					Buffer.from(await imageTest.arrayBuffer())
+				);
 			} else {
 				return {
 					status: 400,
 					body: {
-						error: 'does not accept redirect url',
+						'url-endpoint-status': imageTest.status,
+						'error': 'url endpoint error',
 					},
 				}
-			}
+			} 
+		} else if(imageFile) {
+			writeFileSync(
+				path.resolve(`${dir}/${name}.${imageFile.type.split('/')[1]}`),
+				Buffer.from(await imageFile.arrayBuffer())
+			);
+		} else {
+			return {
+				status: 200,
+				body: {
+					'warn': 'no image',
+				},
+			};
 		}
+		return {
+			status: 200,
+		};
 	} catch (error) {
-		console.error(error);
 		return {
 			status: 500,
 			body: { error },
 		};
 	}
-};
+}
